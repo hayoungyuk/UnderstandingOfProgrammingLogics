37e9d8fd474ce91cb51afbd03c29990f611d4d31
Yuk Ha Young <81849413+yukhayoung@users.noreply.github.com>
2022-07-04 20:50:58 +0900

    Delete Week2 directory
    Delete week2 directory to restart studying understanding of programming language

--- Week2/Lab2_1.c
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main(void){
-    int height=165;
-
-    printf("gilDong-Hong\t%d\n",height);
-}
--- Week2/Lab2_2.c
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main(void){
-    int num = 7;
-    
-    printf("The square number of %d is %d\n", num, num*num);
-    printf("The cube number of %d is %d\n",num, num*num*num);
-}
